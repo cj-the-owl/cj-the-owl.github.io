8cc20210ee44dbdd23d6eb6cf8705905a7d8bf31
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-06-29 09:49:52 +0200

    trying to host this thing

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <navbar-comp/>
+  <router-view/>
+  <footer-comp/>
+</template>
+
+<script>
+import FooterComp from './components/footer-comp.vue';
+  import navbarComp from './components/navbar-comp.vue';
+
+  export default {
+    components: {navbarComp, FooterComp}
+  }
+</script>
+<style>
+
+
+#app {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'India Flower', cursive;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #F5B6E1;
+}
+
+nav {
+  padding: 30px;
+}
+
+</style>
---
+++ src/components/card-comp.vue
@@ -0,0 +1,38 @@
+<template>
+    <router-link :to="{name: 'product', params:{id: product.id}}" style="text-decoration: none;">
+        <img :src="product.image" :alt="product.name" id="product-img">
+        <h3>
+            {{ product.name }}
+        </h3>
+        <p>Price: R {{ product.price }}</p>
+    </router-link>
+</template>
+
+<script>
+    export default {
+        props: ["product"]
+    }
+</script>
+
+<style>
+    * {
+    /* padding: 0; */
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Indie Flower', cursive;
+}
+
+    #product-img {
+    width: 200px;
+    height: 200px;
+}
+
+h3, p {
+    color:black; 
+}
+
+h3:hover {
+    color: #A129C9;
+}
+
+</style>
